6024f52be443aa382aeda2bb5b4840f97a5da061
Yen Huynh <90292203+YenHuynh02@users.noreply.github.com>
2024-10-24 22:21:42 -0400

    Updated

--- 13. RomanToInt/romanInt.js
+++ 13. RomanToInt/romanInt.js
@@ -2,6 +2,33 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
+var romanToInt = function (s) {
+    var roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    let sum = 0;
+
+    for (var i = 0; i < s.length; i++) {
+        if (i + 1 < s.length && roman[s[i]] < roman[s[i+1]]) {
+            sum += roman[s[i + 1]] - roman[s[i]];
+            i++;
+        }
+        else {
+            sum += roman[s[i]];
+        }
+    }
+    return sum;
 };
+
+var romanNum = ['III', 'LVIII', 'MCMXCIV'];
+    romanNum.forEach((num) => {
+        console.log(romanToInt(num));
+    })    
+
