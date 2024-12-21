5e7ea429772f24bc316f9f0f9d1083f50746581b
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2024-12-21 13:46:31 -0500

    Updated

---
+++ Medium/48. Rotate Image/rotateImg.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+    let n = matrix.length;
+    const res = Array.from({ length: n }, () => Array(n).fill(0));
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            res[j][n - i - 1] = matrix[i][j];
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        matrix[i] = res[i].slice();
+    }
+};
+
+const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+rotate(matrix);
+console.log(matrix);
