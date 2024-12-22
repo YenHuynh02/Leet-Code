9adab25d8371033db5e95bb9ae323b95e7faeb6e
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2024-12-21 19:49:06 -0500

    Updated

--- Medium/48. Rotate Image/rotateImg.js
+++ Medium/48. Rotate Image/rotateImg.js
@@ -4,7 +4,6 @@
  */
 var rotate = function (matrix) {
     let n = matrix.length;
-    const res = Array.from({ length: n }, () => Array(n).fill(0));
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             res[j][n - i - 1] = matrix[i][j];
